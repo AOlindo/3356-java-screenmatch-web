afdd17d8c35d9da308d5fa84bac1ab293e8ed43f
Adriana Olindo <adriana-olindo18@hotmail.com>
2024-02-06 17:27:06 -0300

    Exibir séries mais recentes e correção de bug com JPQL

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -4,19 +4,38 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.screenmatch.Dto.SerieDto;
 import br.com.alura.screenmatch.service.SerieService;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
 
 	@Autowired
 	private SerieService serieService;
 	
-	@GetMapping("/series")
+	@GetMapping
 	public List<SerieDto> obterSeries() {
 		return serieService.obterTodasAsSeries();
+	}
+	
+	@GetMapping("/top5")
+	public List<SerieDto> obterTop5Series(){
+		return serieService.obterTop5Series();
+	}
+	
+	@GetMapping("/lancamentos")
+	public List<SerieDto> obterLancamento(){
+		return serieService.obterLancamentos();
+	}
+	
+	@GetMapping("/{id}")
+	public SerieDto obterPorId(@PathVariable Long id){
+		return serieService.obterPorId(id);
+		
 	}
 }
--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -1,12 +1,14 @@
 package br.com.alura.screenmatch.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.screenmatch.Dto.SerieDto;
+import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.repository.SerieRepository;
 
 @Service
@@ -16,10 +18,31 @@ public class SerieService {
 	private SerieRepository serieRepository;
 
 	public List<SerieDto> obterTodasAsSeries() {
-		return serieRepository
-				.findAll().stream().map(s -> new SerieDto(s.getId(), s.getTitulo(), s.getTotalTemporadas(),
-						s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
-				.collect(Collectors.toList());
+		return converteDados(serieRepository.findAll());
+	}
+	
+	public List<SerieDto> obterTop5Series(){
+		return converteDados(serieRepository.findTop5ByOrderByAvaliacaoDesc());
+	}
+	
+	private List<SerieDto> converteDados(List<Serie> series){
+		 return series.stream()
+		            .map(s -> new SerieDto(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+		            .collect(Collectors.toList());
 	}
 
+	public List<SerieDto> obterLancamentos() {
+		return converteDados(serieRepository.encontrarEpisodiosMaisRecentes());
+	}
+	
+	public SerieDto obterPorId(Long id) {
+		Optional<Serie> serie = serieRepository.findById(id);
+		if(serie.isPresent()) {
+			Serie s = serie.get();
+			return new SerieDto(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse());
+		}
+		
+		return null;
+	}
+	
 }
